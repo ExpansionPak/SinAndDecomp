02d06e80b386b1a61d54fc4a0e32e3a76a21867d
Macbook Pro <dreamsailing59@gmail.com>
2026-05-06 19:47:17 +1000

    Initial Commit

---
+++ tsumitobatsu.d
@@ -0,0 +1,172 @@
+build/tsumitobatsu.elf: \
+    build/asm/header.o \
+    build/assets/ipl3.o \
+    build/asm/1000.o \
+    build/asm/boot.o \
+    build/asm/18B50.o \
+    build/asm/18BD0.o \
+    build/asm/18CC0.o \
+    build/asm/196A0.o \
+    build/asm/19730.o \
+    build/asm/19860.o \
+    build/asm/19C50.o \
+    build/asm/19DC0.o \
+    build/asm/19FB0.o \
+    build/asm/1A050.o \
+    build/asm/1A1D0.o \
+    build/asm/1A590.o \
+    build/asm/1A680.o \
+    build/asm/1A730.o \
+    build/asm/1A750.o \
+    build/asm/1A890.o \
+    build/asm/1A930.o \
+    build/asm/1B160.o \
+    build/asm/1BC70.o \
+    build/asm/1BD80.o \
+    build/asm/1CA50.o \
+    build/asm/1CE70.o \
+    build/asm/1DA10.o \
+    build/asm/1DB90.o \
+    build/asm/1DBD0.o \
+    build/asm/1DC20.o \
+    build/asm/1DC60.o \
+    build/asm/1E2A0.o \
+    build/asm/1E2B0.o \
+    build/asm/1E350.o \
+    build/asm/1E480.o \
+    build/asm/1E500.o \
+    build/asm/1E530.o \
+    build/asm/1E7B0.o \
+    build/asm/1EAC0.o \
+    build/asm/1EB20.o \
+    build/asm/1EB30.o \
+    build/asm/1EF00.o \
+    build/asm/1F4D0.o \
+    build/asm/1F600.o \
+    build/asm/1F7C0.o \
+    build/asm/204A0.o \
+    build/asm/20F80.o \
+    build/asm/20FE0.o \
+    build/asm/21120.o \
+    build/asm/21250.o \
+    build/asm/21430.o \
+    build/asm/21450.o \
+    build/asm/214F0.o \
+    build/asm/21500.o \
+    build/asm/21510.o \
+    build/asm/21520.o \
+    build/asm/21530.o \
+    build/asm/21580.o \
+    build/asm/21660.o \
+    build/asm/218A0.o \
+    build/asm/218C0.o \
+    build/asm/21910.o \
+    build/asm/22340.o \
+    build/asm/22390.o \
+    build/asm/223E0.o \
+    build/asm/22490.o \
+    build/asm/22580.o \
+    build/asm/22670.o \
+    build/asm/22820.o \
+    build/asm/22840.o \
+    build/asm/22910.o \
+    build/asm/22A30.o \
+    build/asm/22AF0.o \
+    build/asm/22B30.o \
+    build/asm/22B80.o \
+    build/asm/22C10.o \
+    build/asm/22D40.o \
+    build/asm/23060.o \
+    build/asm/23120.o \
+    build/asm/kernel_end.o \
+    build/assets/main_gap.o \
+    build/asm/engine_start.o \
+    build/asm/engine_logic.o \
+    build/asm/big_chunk.o \
+    build/assets/100000.o
+build/asm/header.o:
+build/assets/ipl3.o:
+build/asm/1000.o:
+build/asm/boot.o:
+build/asm/18B50.o:
+build/asm/18BD0.o:
+build/asm/18CC0.o:
+build/asm/196A0.o:
+build/asm/19730.o:
+build/asm/19860.o:
+build/asm/19C50.o:
+build/asm/19DC0.o:
+build/asm/19FB0.o:
+build/asm/1A050.o:
+build/asm/1A1D0.o:
+build/asm/1A590.o:
+build/asm/1A680.o:
+build/asm/1A730.o:
+build/asm/1A750.o:
+build/asm/1A890.o:
+build/asm/1A930.o:
+build/asm/1B160.o:
+build/asm/1BC70.o:
+build/asm/1BD80.o:
+build/asm/1CA50.o:
+build/asm/1CE70.o:
+build/asm/1DA10.o:
+build/asm/1DB90.o:
+build/asm/1DBD0.o:
+build/asm/1DC20.o:
+build/asm/1DC60.o:
+build/asm/1E2A0.o:
+build/asm/1E2B0.o:
+build/asm/1E350.o:
+build/asm/1E480.o:
+build/asm/1E500.o:
+build/asm/1E530.o:
+build/asm/1E7B0.o:
+build/asm/1EAC0.o:
+build/asm/1EB20.o:
+build/asm/1EB30.o:
+build/asm/1EF00.o:
+build/asm/1F4D0.o:
+build/asm/1F600.o:
+build/asm/1F7C0.o:
+build/asm/204A0.o:
+build/asm/20F80.o:
+build/asm/20FE0.o:
+build/asm/21120.o:
+build/asm/21250.o:
+build/asm/21430.o:
+build/asm/21450.o:
+build/asm/214F0.o:
+build/asm/21500.o:
+build/asm/21510.o:
+build/asm/21520.o:
+build/asm/21530.o:
+build/asm/21580.o:
+build/asm/21660.o:
+build/asm/218A0.o:
+build/asm/218C0.o:
+build/asm/21910.o:
+build/asm/22340.o:
+build/asm/22390.o:
+build/asm/223E0.o:
+build/asm/22490.o:
+build/asm/22580.o:
+build/asm/22670.o:
+build/asm/22820.o:
+build/asm/22840.o:
+build/asm/22910.o:
+build/asm/22A30.o:
+build/asm/22AF0.o:
+build/asm/22B30.o:
+build/asm/22B80.o:
+build/asm/22C10.o:
+build/asm/22D40.o:
+build/asm/23060.o:
+build/asm/23120.o:
+build/asm/kernel_end.o:
+build/assets/main_gap.o:
+build/asm/engine_start.o:
+build/asm/engine_logic.o:
+build/asm/big_chunk.o:
+build/assets/100000.o:
+-include build/asm/header.d build/assets/ipl3.d build/asm/1000.d build/asm/boot.d build/asm/18B50.d build/asm/18BD0.d build/asm/18CC0.d build/asm/196A0.d build/asm/19730.d build/asm/19860.d build/asm/19C50.d build/asm/19DC0.d build/asm/19FB0.d build/asm/1A050.d build/asm/1A1D0.d build/asm/1A590.d build/asm/1A680.d build/asm/1A730.d build/asm/1A750.d build/asm/1A890.d build/asm/1A930.d build/asm/1B160.d build/asm/1BC70.d build/asm/1BD80.d build/asm/1CA50.d build/asm/1CE70.d build/asm/1DA10.d build/asm/1DB90.d build/asm/1DBD0.d build/asm/1DC20.d build/asm/1DC60.d build/asm/1E2A0.d build/asm/1E2B0.d build/asm/1E350.d build/asm/1E480.d build/asm/1E500.d build/asm/1E530.d build/asm/1E7B0.d build/asm/1EAC0.d build/asm/1EB20.d build/asm/1EB30.d build/asm/1EF00.d build/asm/1F4D0.d build/asm/1F600.d build/asm/1F7C0.d build/asm/204A0.d build/asm/20F80.d build/asm/20FE0.d build/asm/21120.d build/asm/21250.d build/asm/21430.d build/asm/21450.d build/asm/214F0.d build/asm/21500.d build/asm/21510.d build/asm/21520.d build/asm/21530.d build/asm/21580.d build/asm/21660.d build/asm/218A0.d build/asm/218C0.d build/asm/21910.d build/asm/22340.d build/asm/22390.d build/asm/223E0.d build/asm/22490.d build/asm/22580.d build/asm/22670.d build/asm/22820.d build/asm/22840.d build/asm/22910.d build/asm/22A30.d build/asm/22AF0.d build/asm/22B30.d build/asm/22B80.d build/asm/22C10.d build/asm/22D40.d build/asm/23060.d build/asm/23120.d build/asm/kernel_end.d build/assets/main_gap.d build/asm/engine_start.d build/asm/engine_logic.d build/asm/big_chunk.d build/assets/100000.d
